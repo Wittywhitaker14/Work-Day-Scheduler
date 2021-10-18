27c68e39a7f721c6ed91e2376daa9094b0ef1a07
Austin Whitaker <wittywhitaker14@gmail.com>
2021-10-17 22:44:39 -0600

    added a class

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,10 @@ var time = $('#currentDay.lead').append(moment().format("dddd, MMMM Do, YYYY"));
 
 
 var form = $("form").on("click", function() {
-        // get current text of p element
         var text = $(this)
           .text()
           .trim();
       
-        // replace p element with a new textarea
         var textInput = $("<input>").addClass("textarea");
         $(this).replaceWith(textInput);
 });
