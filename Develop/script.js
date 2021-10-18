cc3d84c206dd56d072a8e5b4795b075a542e4e4b
Austin Whitaker <wittywhitaker14@gmail.com>
2021-10-18 13:57:04 -0600

    Refactored my index.html to be appended by script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,29 @@
-var list = JSON.parse(localStorage.getItem('tasks')) || [];
-
+var tasks = []
 var time = $('#currentDay.lead').append(moment().format("dddd, MMMM Do, YYYY"));
+var currentHour = moment().hour()
+var timeblocksContainer = $('#container')
+var hours = [9,10,11,12,13,14,15,16,17];
 
+function hourEl(){
+  for (let index = 0; index < hours.length; index++) {
+        
+    var row = $('<div class="row">');
+    var col = $('<div class="col-md-1 hour">');
+    var hoursLabel = hours[index] + "AM";
+    var taskArea = $("<textarea class='col-md-10 textarea input card description' placeholder='Add Event Here' ></textarea>")
+    var saveBtn = $("<button class='col-md-1 saveBtn' type='button' id='save-i+9'><i class='oi oi-file'></i></button>")
 
-var form = $("form").on("click", function() {
-        var text = $(this)
-          .text()
-          .trim();
-      
-        var textInput = $("<input>").addClass("textarea");
-        $(this).replaceWith(textInput);
-});
+    if(hours[index]>= 12){
+      hoursLabel = hours[index] + "PM";
+    }if(hours[index]>= 13){
+      hoursLabel= (hours[index] - 12) + "PM";
+    };
+    row.append(col);
+    col.append(hoursLabel);
+    row.append(taskArea);
+    row.append(saveBtn);
+    $("#container").append(row);
+  }
+};
+
+hourEl();
